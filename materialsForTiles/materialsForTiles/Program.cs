aac9e7f59837b693fd01d9e894bf8982f07275a6
Иван Бахарев <ragna.blood82@gmail.com>
2024-01-14 23:47:40 +0300

    Start

---
+++ materialsForTiles/materialsForTiles/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+//Планируемые затраты на плитку
+
+decimal plannedCostOfTiles1, plannedCostOfTiles2, plannedCostOfTiles3;
+
+{
+    decimal sandForTail1 = 5, sandForTail2 = 3, sandForTail3 = 2;
+}
+
+decimal totalcost = plannedCostOfTiles1 + plannedCostOfTiles2 + plannedCostOfTiles3;
+
+Console.WriteLine($"Общая стоимость сырья = {totalcost}");
